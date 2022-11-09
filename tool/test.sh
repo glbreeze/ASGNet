34a273666549ffcbffa84fce5095373db643f36c
glbreeze <glbreeze@gmail.com>
2022-11-09 16:39:03 +0800

    coco_test

--- tool/test.sh
+++ tool/test.sh
@@ -1,9 +1,22 @@
-#!/bin/sh
-PARTITION=Segmentation
+#!/bin/bash
+
+#SBATCH --job-name=seg
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=20GB
+#SBATCH --time=24:00:00
+#SBATCH --mail-type=END
+#SBATCH --mail-user=lg154@nyu.edu
+#SBATCH --output=seg.out
+#SBATCH --gres=gpu # How much gpu need, n is the number
+
+module purge
 
 dataset=$1
 model_name=$2
 exp_name=$3
+shot=$4
 exp_dir=exp/${model_name}/${dataset}/${exp_name}
 model_dir=${exp_dir}/model
 result_dir=${exp_dir}/result
@@ -13,4 +26,12 @@ mkdir -p ${model_dir} ${result_dir}
 now=$(date +"%Y%m%d_%H%M%S")
 cp tool/test.sh tool/test.py ${config} ${exp_dir}
 
-python -u -m tool.test --config=${config} 2>&1 | tee ${result_dir}/test-$now.log
+
+echo "start"
+singularity exec --nv \
+            --overlay /scratch/lg154/python36/python36.ext3:ro \
+            --overlay /scratch/lg154/sseg/dataset/coco2014.sqf:ro \
+            /scratch/work/public/singularity/cuda11.2.2-cudnn8-devel-ubuntu20.04.sif \
+            /bin/bash -c "source /ext3/env.sh; python -m tool.test --config=${config} > ${result_dir}/test-${shot}shot-$now.log 2>&1"
+echo "finish"
+
